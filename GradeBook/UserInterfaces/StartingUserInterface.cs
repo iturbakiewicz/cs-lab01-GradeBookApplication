d2e8e0fab289f401975b847c276b0ba9ca4be01a
Igor Turbakiewicz <igor.turbakiewicz42@gmail.com>
2024-04-14 21:18:49 +0200

    commit

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,29 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
+            if (parts[2] == "Ranked")
+            {
+                RankedGradeBook gradeBook = new RankedGradeBook(name, bool.Parse(parts[3]));
+                Console.WriteLine("Created gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(gradeBook);
+            }
+            else if (parts[2] == "Standard")
+            {
+                BaseGradeBook gradeBook = new StandardGradeBook(name, bool.Parse(parts[3]));
+                Console.WriteLine("Created gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(gradeBook);
+            }
+            else
+            {
+                Console.WriteLine(parts[2] + "is not supported type of gradebook, please try again");
+                return;
+            }
         }
 
         public static void LoadCommand(string command)
